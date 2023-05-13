0c0be50c30d9e6c85d3b24aa34c7dd45a32721e2
PAULA SOFIA ALBI <albi.paula@gmail.com>
2023-05-13 21:48:59 +0800

    flip-card-done

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+/* 'use strict'
+const gallery = document.querySelector('.gallery');
+const feed= document.querySelector('.contenedor-gallery');
+const next= document.querySelector('#next'); 
+const next= document.querySelector('#prev'); 
+
+const token = 'IGQVJXdDlncGtfNTRBZAk92NjlmaktaZAld1S05xY0Q1dExVWEF4SWdRS3NUeUJXYUZAxNTFiM3NVc3FqZAXg4TWQ4dUJ0TWNOUGVLV1M0UmtmUmtpNFUyYzhXc1k2eEQ2UDQwX3lEcjlMcGhzNEctc1RqaAZDZD';
+const url= 'https://graph.instagram.com/v11.0/10218560180051171/media'; */
