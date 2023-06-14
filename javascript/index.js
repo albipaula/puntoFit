23ce2911be4c34b1c5637e279dc592761dbfd4e9
PAULA SOFIA ALBI <albi.paula@gmail.com>
2023-06-14 13:55:34 +0800

    google-analytycs

--- javascript/index.js
+++ javascript/index.js
@@ -59,11 +59,3 @@ myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
 
-<!-- Google tag (gtag.js) -->
-
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-50MWWRPQSD');
